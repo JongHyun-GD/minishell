45c8dd0a0c083382d1a92e41085ebe1b40ea8f17
sondho <thseogh135@gmail.com>
2021-11-01 10:43:02 +0900

    Add header files

---
+++ includes/minishell.h
@@ -0,0 +1,11 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "list.h"
+# include "../libft/includes/libft.h"
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+
+#endif
