a5f30870ef655c564c7bb0a6d8bbf4b0ad806d5b
JongHyun-GD <58730856+JongHyun-GD@users.noreply.github.com>
2021-11-08 10:51:09 +0900

    Fix norm in echo.c

--- src/echo.c
+++ src/echo.c
@@ -6,7 +6,7 @@
 /*   By: jongpark <jongpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 10:31:57 by jongpark          #+#    #+#             */
-/*   Updated: 2021/11/08 10:48:01 by jongpark         ###   ########.fr       */
+/*   Updated: 2021/11/08 10:50:44 by jongpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ int	echo(t_list *list)
 	bool	has_n_flag;
 	t_node	*node;
 
-	if (!list || list->l_type != LTYPE_COMMAND) return (-1);
-
+	if (!list || list->l_type != LTYPE_COMMAND)
+		return (-1);
 	has_n_flag = false;
 	node = list->start_node;
 	while (node)
 	{
-		if (ft_strlen(node->data) == 2 &&
-			node->data[0] == '-' && node->data[1] == 'n')
+		if (ft_strlen(node->data) == 2
+			&& node->data[0] == '-' && node->data[1] == 'n')
 			has_n_flag = true;
 		else
 			printf("%s", node->data);
@@ -32,4 +32,5 @@ int	echo(t_list *list)
 	}
 	if (has_n_flag == false)
 		printf("\n");
+	return (0);
 }
