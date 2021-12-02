3f880c1b68ec1397692acd1d508021fc6c03e5ea
JongHyun-GD <58730856+JongHyun-GD@users.noreply.github.com>
2021-12-02 15:50:37 +0900

    Implement redirect r1

--- src/execute_builtin.c
+++ src/execute_builtin.c
@@ -6,7 +6,7 @@
 /*   By: hyun <hyun@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 14:31:26 by jongpark          #+#    #+#             */
-/*   Updated: 2021/12/02 14:00:10 by hyun             ###   ########.fr       */
+/*   Updated: 2021/12/02 15:47:43 by hyun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,53 +29,63 @@ bool	is_builtin(char *commandline)
 int	try_exec_builtin(char *commandline, t_list *list, t_info *info)
 {
 	int	fd;
+	int	pid;
+	int	status;
 
+	fd = -1;
 	if (is_builtin(commandline) == false)
 		return (-1);
-	if (info->has_pipe_in)
+	pid = fork();
+	if (pid == 0)
 	{
-		dup2(info->pipe_in[WRITE_END], STDOUT_FILENO);
-		close(info->pipe_in[WRITE_END]);
-		info->has_pipe_in = false;
-	}
-	if (info->has_redirect_r1)
-	{
-		printf("built in: path = %s,\n", info->r1_path);
-		fd = open(info->r1_path, O_WRONLY | O_CREAT, 0755);
-		printf("h1\n");
-		dup2(fd, STDOUT_FILENO);
-		printf("h2\n");
-		close(fd);
-		printf("h3\n");
-
-	}
-	if (list->prev && list->prev->l_type == LTYPE_PIPE)
-	{
-		dup2(info->pipe_out[READ_END], STDIN_FILENO);
-		close(info->pipe_out[READ_END]);
-	}
-	if (ft_strncmp(commandline, "env", ft_strlen(commandline)) == 0)
-		env(info);
-	else if (ft_strncmp(commandline, "export", 6) == 0)
-		ft_export(list, info);
-	else if (ft_strncmp(commandline, "unset", 5) == 0)
-		unset(list, info);
-	else if (ft_strncmp(commandline, "pwd", 3) == 0)
-		pwd();
-	else if (ft_strncmp(commandline, "cd", 2) == 0)
-		cd(list, info);
-	else if (ft_strncmp(commandline, "echo", 4) == 0)
-		echo(list);
-	else if (ft_strncmp(commandline, "exit", 4) == 0)
-	{
-		printf("exit\n");
+		if (info->has_pipe_in)
+		{
+			dup2(info->pipe_in[WRITE_END], STDOUT_FILENO);
+			close(info->pipe_in[WRITE_END]);
+			info->has_pipe_in = false;
+		}
+		if (info->has_redirect_r1)
+		{
+			fd = open(info->r1_path, O_WRONLY | O_CREAT, 0755);
+			dup2(fd, STDOUT_FILENO);
+		}
+		if (list->prev && list->prev->l_type == LTYPE_PIPE)
+		{
+			dup2(info->pipe_out[READ_END], STDIN_FILENO);
+			close(info->pipe_out[READ_END]);
+		}
+		if (ft_strncmp(commandline, "env", ft_strlen(commandline)) == 0)
+			env(info);
+		else if (ft_strncmp(commandline, "export", 6) == 0)
+			ft_export(list, info);
+		else if (ft_strncmp(commandline, "unset", 5) == 0)
+			unset(list, info);
+		else if (ft_strncmp(commandline, "pwd", 3) == 0)
+			pwd();
+		else if (ft_strncmp(commandline, "cd", 2) == 0)
+			cd(list, info);
+		else if (ft_strncmp(commandline, "echo", 4) == 0)
+			echo(list);
+		else if (ft_strncmp(commandline, "exit", 4) == 0)
+		{
+			printf("exit\n");
+			exit(2);
+		}
 		exit(0);
 	}
-	if (info->has_redirect_r1)
+	else
 	{
-		printf("r2\n");
-		info->has_redirect_r1 = false;
+		pid = wait(&status);
+		if (pid < 0)
+			return (-1);
+		if (status >> 8 == 2)
+			exit(0);
+		if (info->has_redirect_r1)
+		{
+			if (fd != -1)
+				close(fd);
+			info->has_redirect_r1 = false;
+		}
 	}
-
 	return (0);
 }
