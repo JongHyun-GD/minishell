25d5eadf9c9d9d2672fbc891d9b6906528f21096
JongHyun-GD <58730856+JongHyun-GD@users.noreply.github.com>
2021-12-15 18:10:03 +0900

    Fix issue that echo -n flag doesn't work well

--- src/echo.c
+++ src/echo.c
@@ -3,33 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dason <dason@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: jongpark <jongpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 10:31:57 by jongpark          #+#    #+#             */
-/*   Updated: 2021/12/15 17:36:37 by dason            ###   ########.fr       */
+/*   Updated: 2021/12/15 18:09:46 by jongpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/echo.h"
 
+t_node	*check_flag(t_node *start_node, bool *has_n_flag)
+{
+	char	*data;
+
+	if (start_node->next)
+	{
+		data = start_node->next->data;
+		if (ft_strlen(data) == 2
+			&& data[0] == '-' && data[1] == 'n')
+		{
+			*has_n_flag = true;
+			return (start_node->next->next);
+		}
+	}
+	*has_n_flag = false;
+	return (start_node->next);
+}
+
 int	echo(t_list *list, t_info *info)
 {
 	bool	has_n_flag;
 	t_node	*node;
+	int		node_count;
 
 	if (!list || list->l_type != LTYPE_COMMAND)
 		return (-1);
-	has_n_flag = false;
-	node = list->start_node->next;
-	while (node)
+	node = check_flag(list->start_node, &has_n_flag);
+	node_count = 0;
+	while (node && ++node_count)
 	{
-		if (node != list->start_node->next)
-			printf(" ");
-		if (ft_strlen(node->data) == 2
-			&& node->data[0] == '-' && node->data[1] == 'n')
-			has_n_flag = true;
-		else
-			printf("%s", node->data);
+		if (node_count != 1)
+			write(STDOUT_FILENO, " ", 1);
+		write(STDOUT_FILENO, node->data, ft_strlen(node->data));
 		node = node->next;
 	}
 	if (has_n_flag == false)
