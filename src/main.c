3520bade3af359bcc659aad12d18ff36e10b4bff
JongHyun-GD <58730856+JongHyun-GD@users.noreply.github.com>
2021-12-24 16:26:08 +0900

    Fix typo error

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jongpark <jongpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 14:02:32 by dason             #+#    #+#             */
-/*   Updated: 2021/12/24 16:17:27 by jongpark         ###   ########.fr       */
+/*   Updated: 2021/12/24 16:25:51 by jongpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ int	move_to_next_command_list(t_list **list)
 	has_next = -1;
 	*list = (*list)->next;
 	while (((*list) && (*list)->l_type != LTYPE_COMMAND)
-		|| (*list && (*list)->prev->l_type != LTYPE_PIPE))
+		&& (*list && (*list)->prev->l_type != LTYPE_PIPE))
 	{
 		has_next = 0;
 		*list = (*list)->next;
