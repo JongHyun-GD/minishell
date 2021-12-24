d7b1d1d948c1cc0b1c69390fb02c4af8cb3db770
sondho <thseogh135@gmail.com>
2021-12-24 14:21:27 +0900

    Fix segmentaion fault (If there's no LTYPE_FILE after redirection. If there's no node after redirection.)

--- src/parser_handling_exception.c
+++ src/parser_handling_exception.c
@@ -6,38 +6,66 @@
 /*   By: dason <dason@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 09:44:28 by dason             #+#    #+#             */
-/*   Updated: 2021/12/13 09:44:29 by dason            ###   ########.fr       */
+/*   Updated: 2021/12/24 14:20:11 by dason            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/parser.h"
 
-static void	print_parse_error_near(int l_type)
+static bool	check_parse_error_near(t_list *list)
 {
-	if (l_type == 2)
-		printf("minishell: syntax error near unexpected token `|'\n");
-	if (l_type == 3)
-		printf("minishell: syntax error near unexpected token `<'\n");
-	if (l_type == 4)
-		printf("minishell: syntax error near unexpected token `<<'\n");
-	if (l_type == 5)
-		printf("minishell: syntax error near unexpected token `>'\n");
-	if (l_type == 6)
-		printf("minishell: syntax error near unexpected token `>>'\n");
+	int		l_type;
+
+	if (list && list->next && \
+		list->l_type != LTYPE_COMMAND && \
+		list->l_type != LTYPE_FILE && \
+		list->next->l_type != LTYPE_COMMAND && \
+		list->next->l_type != LTYPE_FILE)
+	{
+		l_type = list->next->l_type;
+		if (l_type == 2)
+			printf("minishell: syntax error near unexpected token `|'\n");
+		if (l_type == 3)
+			printf("minishell: syntax error near unexpected token `<'\n");
+		if (l_type == 4)
+			printf("minishell: syntax error near unexpected token `<<'\n");
+		if (l_type == 5)
+			printf("minishell: syntax error near unexpected token `>'\n");
+		if (l_type == 6)
+			printf("minishell: syntax error near unexpected token `>>'\n");
+		return (false);
+	}
+	return (true);
 }
 
-int	parser_handling_exception(t_list *list)
+static bool	check_non_path_and_non_eof(t_list *list)
 {
 	while (list)
 	{
 		if (list->l_type != LTYPE_COMMAND && \
 			list->l_type != LTYPE_FILE && \
-			list->next->l_type != LTYPE_COMMAND && \
-			list->next->l_type != LTYPE_FILE)
+			(!list->next || \
+			list->next->l_type != LTYPE_FILE))
 		{
-			print_parse_error_near(list->next->l_type);
-			return (0);
+			printf("minishell: syntax error near unexpected token `newline'\n");
+			return (false);
 		}
+		list = list->next;
+	}
+	return (true);
+}
+
+int	parser_handling_exception(t_list *list)
+{
+	t_list		*work_list;
+
+	while (list)
+	{
+		work_list = list;
+		if (check_parse_error_near(work_list) == false)
+			return (0);
+		if (check_non_path_and_non_eof(work_list) == false)
+			return (0);
 		list = list->next;
 	}
 	return (1);
